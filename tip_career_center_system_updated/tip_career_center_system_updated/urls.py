0a36edea883b5a808f7dda9921642179f9dfabbc
Royce <royce236@gmail.com>
2019-09-14 15:19:22 +0800

    removed admin link

--- tip_career_center_system_updated/tip_career_center_system_updated/urls.py
+++ tip_career_center_system_updated/tip_career_center_system_updated/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('',include('login.urls')),
     path('company-management/',include('company_management.urls')),
 ]
