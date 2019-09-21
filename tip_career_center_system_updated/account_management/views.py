b8f81aad60034fe032f45226e4400568f7ecf0f9
unknown <joshuaalbertlopez0378@gmail.com>
2019-09-21 22:01:59 +0800

    Updated registration

--- tip_career_center_system_updated/account_management/views.py
+++ tip_career_center_system_updated/account_management/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.db import connection
 from django.contrib.auth.hashers import make_password, check_password
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here.
 def dictfetchall(cursor): 
@@ -37,28 +38,39 @@ class Login(View):
         else:    
             return redirect('/')
 
-class Register(View):
+class Register(SuccessMessageMixin,View):
     #(test) login_url = '/' if LoginRequiredMixin used for restriction, add to parameter 
     #redirect_field_name = 'Login First'
     def get(self, request, *args, **kwargs):
         return render(request,template_name='account_management/signup.html',context={})
 
-        
     def post(self, request, *args, **kwargs):
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get ("last_name")
         username = request.POST.get("username") 
         password = request.POST.get("password")
         password2 = request.POST.get("password2")
+        email = request.POST.get("email")
         isAdmin = request.POST.get("isAdmin")
 
-        if password == password2:
-            hashed_pass = make_password(password)
-
-            with connection.cursor() as cursor:
-                sql = "INSERT INTO accounts(username, password, isAdmin) VALUES ('{}','{}','{}')".format(username,hashed_pass,isAdmin)
-                cursor.execute(sql)
-                connection.commit()
-            return redirect('/')
+        with connection.cursor() as cursor:
+            sql = "SELECT username FROM accounts ".format(username)
+            cursor.execute(sql)
+            result = dictfetchall(cursor)
+        if {'username':username} in result:
+            print ("Username exists")
+            return redirect ("http://127.0.0.1:8000/registerAdmin/")
+            
         else:
-            print('Pass no match')
-            return redirect('/registerAdmin/')
-
+            #if password == password2:
+                hashed_pass = make_password(password)
+                with connection.cursor() as cursor:
+                    sql = "INSERT INTO accounts(first_name,last_name,email,username, password, isAdmin) VALUES ('{}','{}','{}','{}','{}','{}')".format(first_name,last_name, email, username,hashed_pass,isAdmin)
+                    cursor.execute(sql)
+                    connection.commit()
+                success_message = "%(username)s was created successfully"
+                return redirect('/')
+                
+           # else:
+               # print ("pass not match")
+              #  return redirect ("http://127.0.0.1:8000/registerAdmin/")
